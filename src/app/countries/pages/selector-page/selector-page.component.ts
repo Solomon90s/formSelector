28363b4d1fbbfbef82a94303b0e367fcbbc48b08
Solomon90s <jose.galdon12@gmail.com>
2024-07-09 17:00:35 +0200

    Comienzo app

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
+
+@Component({
+  selector: 'app-selector-page',
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent {
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  public myForm: FormGroup = this.formBuilder.group({
+    region:  ['', Validators.required ],
+    country: ['', Validators.required ],
+    borders: ['', Validators.required ],
+
+  })
+
+}
